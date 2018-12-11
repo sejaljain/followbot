267eedacce50571f7ff4f021bf428ce7d12f6fa7
Sejal Jain <jain.sejal97@gmaikl.com>
2018-12-11 14:00:25 -0500

    working turns,not stopping on red

--- part2.py
+++ part2.py
@@ -15,88 +15,8 @@ class Follower:
     self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop',
                                        Twist, queue_size=1)
     self.twist = Twist()
-    self.searching_blue = False
-    self.searching_green = True
-    self.searching_red = True
-
-
-  def rotate(self, a, clockwise):
-    #Starts a new node
-
-    # Receiveing the user's input
-    print("Let's rotate your robot")
-    speed = 30
-    angle = a
-
-    #Converting from angles to radians
-    angular_speed = speed*2*math.pi/360
-    relative_angle = angle*2*math.pi/360
-    #print(angular_speed, relative_angle)
-
-    #We wont use linear components
-    self.twist.linear.x=0
-    self.twist.linear.y=0
-    self.twist.linear.z=0
-    self.twist.angular.x = 0
-    self.twist.angular.y = 0
-    r = rospy.Rate(50)
-
-    # Checking if our movement is CW or CCW
-    if clockwise:
-        self.twist.angular.z = -abs(angular_speed)
-    else:
-        self.twist.angular.z = abs(angular_speed)
-    #print(self.twist.angular.z)
-    # Setting the current time for distance calculus
-    t0 = rospy.Time.now().to_sec()
-    current_angle = 0
-    #print(t0)
-    i = 0
-    while(current_angle < relative_angle):
-      i += 1
-      #print(i)
-      #print(current_angle)
-      self.cmd_vel_pub.publish(self.twist)
-      t1 = rospy.Time.now().to_sec()
-      current_angle = angular_speed*(t1-t0)
-      r.sleep()
-    print("stopping")
-    self.twist = Twist()
-    self.cmd_vel_pub.publish(self.twist)
-    print("going straight")
-    self.twist = Twist()
-    self.twist.linear.x = 0.2
-    x = 0
-    while (x < 30):
-      x += 1
-      self.cmd_vel_pub.publish(self.twist)
-      r.sleep()
-    print("turning other way")
-    self.twist = Twist()
-    if clockwise:
-        self.twist.angular.z = abs(angular_speed)
-    else:
-        self.twist.angular.z = -abs(angular_speed)
-
-    t0 = rospy.Time.now().to_sec()
-    current_angle = 0
-    #print(t0)
-    i = 0
-    while(current_angle < relative_angle):
-      i += 1
-      #print(i)
-      #print(current_angle)
-      self.cmd_vel_pub.publish(self.twist)
-      t1 = rospy.Time.now().to_sec()
-      current_angle = angular_speed*(t1-t0)
-      r.sleep()
-
-    print("never do it")
-
-    #Forcing our robot to stop
-    self.twist.angular.z = 0
-    self.cmd_vel_pub.publish(self.twist)
-    #rospy.spin()
+ 
+    self.stop = False
 
 
   def image_callback(self, msg):
@@ -104,17 +24,19 @@ class Follower:
     image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     # yellow bounds are definitely correct
-    lower_yellow = numpy.array([ 15,  0,  0])
-    upper_yellow = numpy.array([36, 255, 250])
-    lower_red = numpy.array([ 0,  0,  0])
-    upper_red = numpy.array([15, 255, 250])
+    lower_yellow = numpy.array([ 10,  10,  10])
+    upper_yellow = numpy.array([30, 255, 250])
+
+    # red bounds right
+    lower_red = numpy.array([ 0,  10,  10])
+    upper_red = numpy.array([10, 255, 250])
 
     # green bounds are definitely correct
-    lower_green = numpy.array([20,10,10])
-    upper_green = numpy.array([70, 255,255])
+    lower_green = numpy.array([50,10,10])
+    upper_green = numpy.array([80, 255,255])
 
     # blue bounds are definitely correct
-    lower_blue = numpy.array([ 75,  10,  10])
+    lower_blue = numpy.array([ 110,  10,  10])
     upper_blue = numpy.array([130, 255, 250])
 
     yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
@@ -123,53 +45,27 @@ class Follower:
     blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
     
     h, w, d = image.shape
-    search_under = 10*h/11
-    search_under_top = 10*h/11 + 20
+
     search_top = 3*h/4
     search_bot = 3*h/4 + 20
     
     yellow_mask[0:search_top, 0:w] = 0
     yellow_mask[search_bot:h, 0:w] = 0
-    
-    red_mask[0:search_under, 0:w] = 0
-    red_mask[search_under_top:h, 0:w] = 0
-    
-    green_mask[0:search_under, 0:w] = 0
-    green_mask[search_under_top:h, 0:w] = 0
-    
-    blue_mask[0:search_under, 0:w] = 0
-    blue_mask[search_under_top:h, 0:w] = 0
+    red_mask[0:search_top, 0:w] = 0
+    red_mask[search_bot:h, 0:w] = 0
+    green_mask[0:search_top, 0:w] = 0
+    green_mask[search_bot:h, 0:w] = 0
+    blue_mask[0:search_top, 0:w] = 0
+    blue_mask[search_bot:h, 0:w] = 0
 
     
     M_yellow = cv2.moments(yellow_mask)
     M_red = cv2.moments(red_mask)
     M_green = cv2.moments(green_mask)
     M_blue = cv2.moments(blue_mask)
-    #print("yellow: ", numpy.sum(yellow_mask))
-    print("green: ", numpy.sum(green_mask))
-    #print("red: ", numpy.sum(red_mask))
-    #print("blue: ", numpy.sum(blue_mask))
-   
-    if M_blue['m00'] > 0 and self.searching_blue:
-        print("Blue found, TURNING")
-        cx = int(M_blue['m10']/M_blue['m00'])
-        cy = int(M_blue['m01']/M_blue['m00'])
-        cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-        self.rotate(90, True)
-        self.searching_blue = False
-        self.searching_green = True
 
-    elif numpy.sum(green_mask) > 45000 and self.searching_green:
-        print("Green found, TURNING")
-        cx = int(M_green['m10']/M_green['m00'])
-        cy = int(M_green['m01']/M_green['m00'])
-        cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-        self.rotate(90, False)
-        self.searching_green = False
-        self.searching_blue = True
-
-
-    elif M_yellow['m00'] > 0:
+    # follow yellow path
+    if not self.stop and M_yellow['m00'] > 0 and M_blue['m00'] == 0 and M_green['m00'] == 0:
       cx = int(M_yellow['m10']/M_yellow['m00'])
       cy = int(M_yellow['m01']/M_yellow['m00'])
       cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
@@ -179,6 +75,58 @@ class Follower:
       self.twist.angular.z = -float(err) / 100
       self.cmd_vel_pub.publish(self.twist)
       # END CONTROL
+
+    # stopped at red block
+    elif self.stop and M_yellow['m00'] > 0 and M_blue['m00'] == 0 and M_green['m00'] == 0:
+      cx = int(M_yellow['m10']/M_yellow['m00'])
+      cy = int(M_yellow['m01']/M_yellow['m00'])
+      cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+      # BEGIN CONTROL
+      err = cx - w/2
+      self.twist.linear.x = 0.6
+      self.twist.angular.z = -float(err) / 100
+      self.cmd_vel_pub.publish(self.twist)
+      rospy.signal_shutdown('red object ecountered')
+      # END CONTROL
+
+    # turn right
+    elif not self.stop and M_blue['m00'] > 0 and M_green['m00'] == 0:
+      print('sensed a blue object, turning right')
+      cx = int(M_yellow['m10']/M_yellow['m00'])
+      cy = int(M_yellow['m01']/M_yellow['m00'])
+      cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+      # BEGIN CONTROL
+      err = cx - w/2
+      self.twist.linear.x = 0.2
+      self.twist.angular.z = -float(err) / 100 - (2*math.pi)/3
+      self.cmd_vel_pub.publish(self.twist)
+      # END CONTROL
+
+    # turn left
+    elif not self.stop and M_blue['m00'] == 0 and M_green['m00'] > 0:
+      print('sensed a green object, turning left')
+      cx = int(M_yellow['m10']/M_yellow['m00'])
+      cy = int(M_yellow['m01']/M_yellow['m00'])
+      cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+      # BEGIN CONTROL
+      err = cx - w/2
+      self.twist.linear.x = 0.2
+      self.twist.angular.z = -float(err) / 100 + (2*math.pi)/3
+      self.cmd_vel_pub.publish(self.twist)
+      # END CONTROL
+
+    elif M_red['m00'] > 0:
+      cx = int(M_yellow['m10']/M_yellow['m00'])
+      cy = int(M_yellow['m01']/M_yellow['m00'])
+      cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+      # BEGIN CONTROL
+      err = cx - w/2
+      self.twist.linear.x = 0.2
+      self.twist.angular.z = -float(err) / 100
+      self.cmd_vel_pub.publish(self.twist)
+      self.stop = True
+
+
     cv2.imshow("window", image)
     cv2.waitKey(3)
 
